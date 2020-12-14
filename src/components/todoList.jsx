3debd8fba125827ccd515f38f00e4a13d945e22a
Edwin <ecelini@macbook-pro-de-edwin.home>
2020-12-14 08:41:18 +0100

    feat: Adding delete a todo

--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,9 +1,9 @@
 import { useCallback } from "react";
-import { connect, useSelector, useDispatch } from "react-redux";
-import { toggleTodoAction } from "../store/todoActions";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTodoAction, deleteTodoAction } from "../store/todoActions";
 import { todoSelector } from "../store/todoSelectors";
 
-function TodoItem({ todo, onToggle }) {
+function TodoItem({ todo, onToggle, onDelete }) {
   return (
     <li>
       <label>
@@ -12,17 +12,22 @@ function TodoItem({ todo, onToggle }) {
           checked={todo.completed}
           onChange={() => onToggle(todo)}
         />
-        {todo.title}
+        {todo.title} <button onClick={() => onDelete(todo)}>x</button>
       </label>
     </li>
   );
 }
 
-export function TodoList({ todos, onToggle }) {
+export function TodoList({ todos, onToggle, onDelete }) {
   return (
     <ul>
       {todos.map((todo) => (
-        <TodoItem todo={todo} onToggle={onToggle} key={todo.id} />
+        <TodoItem
+          todo={todo}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          key={todo.id}
+        />
       ))}
     </ul>
   );
@@ -30,14 +35,24 @@ export function TodoList({ todos, onToggle }) {
 
 export function TodoListStore() {
   const todos = useSelector(todoSelector);
+
   const dispatch = useDispatch();
+
   const onToggle = useCallback(
     (todo) => {
       dispatch(toggleTodoAction(todo));
     },
     [dispatch]
   );
-  return <TodoList todos={todos} onToggle={onToggle} />;
+
+  const onDelete = useCallback(
+    (todo) => {
+      dispatch(deleteTodoAction(todo));
+    },
+    [dispatch]
+  );
+
+  return <TodoList todos={todos} onToggle={onToggle} onDelete={onDelete} />;
 }
 
 // By using todoSelector() & toggleTodoAction() functions, i can export the logic
--- src/store/todoReducer.js
+++ src/store/todoReducer.js
@@ -15,7 +15,7 @@ const initialState = [
 
 export const ADD_TODO_ACTION = "ADD_TODO_ACTION";
 export const UPDATE_TODO_ACTION = "UPDATE_TODO_ACTION";
-
+export const DELETE_TODO_ACTION = "DELETE_TODO_ACTION";
 /**
  * @param {array} state
  * @param {object} action
@@ -32,6 +32,8 @@ export function ToDoReducer(state = initialState, action) {
           return todo;
         }
       });
+    case DELETE_TODO_ACTION:
+      return state.filter((todo) => todo.id !== action.payload);
     default:
       return state;
   }
