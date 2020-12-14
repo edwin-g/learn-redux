bb848d0d33a3e9a971099ac7a4a4e547182a0870
Edwin <ecelini@macbook-pro-de-edwin.home>
2020-12-14 03:18:16 +0100

    refactor: Use of hooks in component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Provider } from "react-redux";
 import store from "./store";
-import { TodoListStore } from "./components/todoList";
+import { TodoList } from "./components/todoList";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <h1>Learn Redux</h1>
-        <TodoListStore />
+        <TodoList />
       </div>
     </Provider>
   );
--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,4 +1,5 @@
-import { connect } from "react-redux";
+import { useCallback } from "react";
+import { connect, useSelector, useDispatch } from "react-redux";
 import { toggleTodoAction } from "../store/todoActions";
 import { todoSelector } from "../store/todoSelectors";
 
@@ -17,7 +18,17 @@ function TodoItem({ todo, onToggle }) {
   );
 }
 
-export function TodoList({ todos, onToggle }) {
+export function TodoList() {
+  // Use of hooks to connect the component to the store
+  const todos = useSelector(todoSelector);
+  const dispatch = useDispatch();
+  const onToggle = useCallback(
+    (todo) => {
+      dispatch(toggleTodoAction(todo));
+    },
+    [dispatch]
+  );
+
   return (
     <ul>
       {todos.map((todo) => (
@@ -29,11 +40,11 @@ export function TodoList({ todos, onToggle }) {
 
 // By using todoSelector() & toggleTodoAction() functions, i can export the logic
 // and making change easly if it's in several component.
-export const TodoListStore = connect(
-  (state) => ({
-    todos: todoSelector(state),
-  }),
-  (dispatch) => ({
-    onToggle: (todo) => dispatch(toggleTodoAction(todo)),
-  })
-)(TodoList);
+// export const TodoListStore = connect(
+//   (state) => ({
+//     todos: todoSelector(state),
+//   }),
+//   (dispatch) => ({
+//     onToggle: (todo) => dispatch(toggleTodoAction(todo)),
+//   })
+// )(TodoList);
